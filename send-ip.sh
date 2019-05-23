9a4290e1a3efe967af0c25e5ad10c2bf7f8a85b0
David Beauchemin <david.beauchemin.5@ulaval.ca>
2019-05-23 16:37:45 -0400

    Webhook initial developpement (#1)
    * added send-ip.service code to run send-ip.sh
    
    * move to system directory and added timer hourly
    
    * added genreic code to push to webhook
    
    * added hostname in ip message
    
    * installation readme

---
+++ send-ip.sh
@@ -0,0 +1,15 @@
+#!/bin/bash
+
+
+ACTUAL_IP=$(hostname -I | awk '{print $1}')
+PREVIOUS_IP=$(cat "$HOME/.myips/$(hostname).txt")
+
+echo $PREVIOUS_IP
+
+if [[ $ACTUAL_IP != $PREVIOUS_IP ]] || [[ -z "$PREVIOUS_IP" ]]; then
+    content="The new IP address of $(hostname) is $ACTUAL_IP"
+    curl -X POST -H 'Content-type: application/json' --data '{"text": "'"$content"'"}' {WEBHOOK_LINK}
+    # write the new ip address
+    hostname -I | awk '{print $1}' >> "$HOME/.myips/despacitoV3-xps.txt"
+fi
+
