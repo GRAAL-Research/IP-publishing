e213a371937427aa5059104ce38caa4885f9eeac
David Beauchemin <david.beauchemin.5@ulaval.ca>
2019-05-23 16:53:16 -0400

    hardcoded hostname in send-ip.sh

--- send-ip.sh
+++ send-ip.sh
@@ -10,6 +10,6 @@ if [[ $ACTUAL_IP != $PREVIOUS_IP ]] || [[ -z "$PREVIOUS_IP" ]]; then
     content="The new IP address of $(hostname) is $ACTUAL_IP"
     curl -X POST -H 'Content-type: application/json' --data '{"text": "'"$content"'"}' {WEBHOOK_LINK}
     # write the new ip address
-    hostname -I | awk '{print $1}' >> "$HOME/.myips/despacitoV3-xps.txt"
+    hostname -I | awk '{print $1}' >> "$HOME/.myips/$(hostname).txt"
 fi
 
