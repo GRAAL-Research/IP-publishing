2451163371116fad9eb9aa89aa2364fe0b758c83
davebulaval <david.beauchemin.5@ulaval.ca>
2019-05-24 11:54:32 -0400

    added root verification

--- install.sh
+++ install.sh
@@ -3,7 +3,8 @@
 
 if [ -z "$1" ]; then
     echo "Error: Missing url argument."
-
+elif ! [ $(id -u) = 0 ]; then
+    echo "You must run the script as root."
 else
     cp system/* /etc/systemd/system/
     cp send-ip.sh /usr/local/bin/send-ip.sh
--- uninstall.sh
+++ uninstall.sh
@@ -1,9 +1,13 @@
 #!/bin/bash
 
-systemctl stop send-ip.service
-systemctl disable send-ip.timer
+if ! [ $(id -u) = 0 ]; then
+    echo "You must run the script as root."
+else
+    systemctl stop send-ip.service
+    systemctl disable send-ip.timer
 
-rm -f /etc/systemd/system/send-ip.*
-rm -f /usr/local/bin/send-ip.sh
-rm -f /var/local/send-ip.txt
-rm -f /usr/local/etc/send-ip
+    rm -f /etc/systemd/system/send-ip.*
+    rm -f /usr/local/bin/send-ip.sh
+    rm -f /var/local/send-ip.txt
+    rm -f /usr/local/etc/send-ip
+fi
