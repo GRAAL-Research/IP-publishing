1ba7e43c3c3ae2bec35d84a1017360226ccfdb01
davebulaval <david.beauchemin.5@ulaval.ca>
2019-05-24 10:24:54 -0400

    modification of ip_file path

--- send-ip.sh
+++ send-ip.sh
@@ -1,15 +1,13 @@
 #!/bin/bash
-
+IP_FILE=$(/var/local/send-ip.txt)
 
 ACTUAL_IP=$(hostname -I | awk '{print $1}')
-PREVIOUS_IP=$(cat "$HOME/.myips/$(hostname).txt")
-
-echo $PREVIOUS_IP
+PREVIOUS_IP=$(cat "$IP_FILE")
 
 if [[ $ACTUAL_IP != $PREVIOUS_IP ]] || [[ -z "$PREVIOUS_IP" ]]; then
     content="The new IP address of $(hostname) is $ACTUAL_IP"
-    curl -X POST -H 'Content-type: application/json' --data '{"text": "'"$content"'"}' $(cat /usr/local/etc/send-ip)
+    curl -X POST -H 'Content-type: application/json' --data '{"text": "'"$content"'"}' $(cat /usr/local/   etc/send-ip)
     # write the new ip address
-    hostname -I | awk '{print $1}' >> "$HOME/.myips/$(hostname).txt"
+    hostname -I | awk '{print $1}' >> "$IP_FILE"
 fi
 
