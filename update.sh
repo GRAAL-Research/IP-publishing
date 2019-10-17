d1dfdbe27fec433bcaabc6c418b98efcca36c226
Frédérik Paradis <fredy_14@live.fr>
2019-10-17 14:31:36 -0400

    Add update script and fix uninstall script with deamon-reload

--- update.sh
+++ update.sh
@@ -6,8 +6,15 @@ else
     systemctl disable send-ip.timer
     rm -f /etc/systemd/system/send-ip.*
     systemctl daemon-reload
-
     rm -f /usr/local/bin/send-ip.sh
-    rm -f /var/local/send-ip.txt
-    rm -f /usr/local/etc/send-ip
+
+    cp system/* /etc/systemd/system/
+    cp send-ip.sh /usr/local/bin/send-ip.sh
+
+    if [ -n "$1" ]; then
+        echo $1 > /usr/local/etc/send-ip
+    fi
+
+    systemctl start send-ip.service
+    systemctl enable send-ip.timer
 fi
