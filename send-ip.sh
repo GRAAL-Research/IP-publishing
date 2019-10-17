5742a8b2ffc637bcd4b29b50ac6c354a07b0b9cd
Frédérik Paradis <fredy_14@live.fr>
2019-10-17 14:17:10 -0400

    Fix when IP is not acquired yet

--- send-ip.sh
+++ send-ip.sh
@@ -7,6 +7,10 @@ PREVIOUS_IP=$(cat "$IP_FILE")
 echo "Previous IP: $PREVIOUS_IP"
 echo "Actual IP: $ACTUAL_IP"
 
+if [[ -z "$ACTUAL_IP" ]]; then
+    exit 0
+fi
+
 if [[ $ACTUAL_IP != $PREVIOUS_IP ]] || [[ -z "$PREVIOUS_IP" ]]; then
     echo "Sending new address to Slack"
     content="The new IP address of $(hostname) is $ACTUAL_IP"
